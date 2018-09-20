e49fc41e318fbd17ba42d6aa76c3722d17c66b2e
jean7rafael <jean7rafael@outlook.com>
2018-09-19 21:50:44 -0300

    troca de site

---
+++ admin.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="pt-br">
+    <title>Setor de Tecnologia UFPR</title>
+	<meta charset="utf-8" />
+    <meta name="description" content="Setor de Tecnologia<br />Centro Politécnico, Ed. de Administração, 2º Andar, Curitiba/PR<br />email: direcao@tecnologia.ufpr.br <br />tel: +55(41)3361-3497" />
+    <meta name="keywords" content="setor, tecnologia, ufpr" />
+	<meta name="author" content="Jean Rafael Martins" />
+	<link rel="stylesheet" href="css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="restrito/css/estilo.index.css" />
+</head>
+
+<body>
+	<div id="login">
+		<img src="images/adminback.png"/>
+		<form action="restrito/logado.php" method="post"> 
+			<h1>senha de administrador</h1>	
+			<input id="box" type="password" name="login-pass" />
+			<input id="enviar" type="submit" value="ir" />
+		</form>
+		
+	</div>
+</body>
+</html>
+
---
+++ restrito/logar.php
@@ -0,0 +1,9 @@
+﻿<?php
+	$senha=$_POST['login-pass'];
+	$cryptpass = crypt("jean");
+	if(crypt($senha, $cryptpass) == $cryptpass){
+		session_start();
+		$_SESSION['logado'] = true;
+		header("location: restrito/index.html");
+	}
+?>
